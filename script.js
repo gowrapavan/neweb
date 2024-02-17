e04f1ff10de8ff1d701f07e7ccb158693a8e7ba6
gowrapavan <160145497+gowrapavan@users.noreply.github.com>
2024-02-17 09:56:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ document.getElementById('login-form').addEventListener('submit', function(event)
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  if (email === '218x1a1227@khitguntur.ac.in' && password === '218x1a1227' || email === '218x1a1244@khitguntur.ac.in' && password === '218x1a1244' || email === '218x1a1236@khitguntur.ac.in' && password === '218x1a1236' || email === '218x1a1207@khitguntur.ac.in' && password === '218x1a1207' ) {
-    window.location.href = 'https://nothing10.mydurable.com/';
+  const validEmails = ['218x1a1227@khitguntur.ac.in', '218x1a1244@khitguntur.ac.in', '218x1a1236@khitguntur.ac.in', '218x1a1207@khitguntur.ac.in'];
+  const validPasswords = ['218x1a1227', '218x1a1244', '218x1a1236', '218x1a1207'];
+
+  if (validEmails.includes(email) && validPasswords.includes(password)) {
+    window.location.href = 'demo.html';
   } else {
     alert('Invalid Email or Password');
   }
