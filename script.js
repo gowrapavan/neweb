6f95ef5eb7f9529f202534f60f9dde8bad9dc9be
gowrapavan <160145497+gowrapavan@users.noreply.github.com>
2024-02-17 09:24:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
+  if (email === '218x1a1227@khitguntur.ac.in' && password === '218x1a1227' || email === '218x1a1244@khitguntur.ac.in' && password === '218x1a1244' || email === '218x1a1236@khitguntur.ac.in' && password === '218x1a1236' || email === '218x1a1207@khitguntur.ac.in' && password === '218x1a1207' ) {
+    window.location.href = 'https://nothing10.mydurable.com/';
+  } else {
+    alert('Invalid Email or Password');
+  }
+});
